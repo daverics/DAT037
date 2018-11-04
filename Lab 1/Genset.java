fd4d73b35218867e2027e20f5645262a83f07718
daverics <david.a.ericsson@gmail.com>
2018-11-04 18:12:21 +0100

    Downloaded files

---
+++ Lab 1/Genset.java
@@ -0,0 +1,7 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>302 Found</title>
+</head><body>
+<h1>Found</h1>
+<p>The document has moved <a href="http://www.cse.chalmers.se/edu/year/2017/course/DAT037/lab1/Genset.java">here</a>.</p>
+</body></html>
